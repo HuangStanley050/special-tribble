81716bd7a9b9a16794cdc819fca06fff42410e56
Stanley Huang <huang.stanley050@gmail.com>
2020-01-06 13:43:45 +1100

    update to some variable names

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-
+  const DELAY = 1000;
   useEffect(() => {
     const interval = setInterval(() => {
       setCount(count + 1);
-    }, 1000);
+    }, DELAY);
 
     return () => {
       clearInterval(interval);
--- src/components/Data.js
+++ src/components/Data.js
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Data = () => {
+  const DELAY = 10000;
   const [fact, setFact] = useState("");
   useEffect(() => {
     const interval = setInterval(async () => {
       let counter = Math.floor(Math.random() * 100) + 1;
       let result = await axios.get(`http://numbersapi.com/${counter}`);
-      //console.log(result.data);
 
       setFact(result.data);
-    }, 10000);
+    }, DELAY);
     return () => {
       clearInterval(interval);
     };
